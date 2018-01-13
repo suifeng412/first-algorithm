ccfbe1041d04ddf652f2f7ced3dc79f9c0a4548c
forever2015412 <3101424353@qq.com>
2018-01-13 09:51:47 +0800

    添加过程解析
    第一题

--- 01-search_in_two_array/search_in_two_array.php
+++ 01-search_in_two_array/search_in_two_array.php
@@ -1,5 +1,5 @@
 <?php
-
+// 第一种方法
 function Find($target, $array)
 {
     foreach($array as  $v){
@@ -18,6 +18,22 @@ while(fscanf(STDIN, "%d,%s", $a,$b) == 2){
     }catch(Exception $e){
         die;
     }
+}
+
+
+// 第二种方法
+function Find($target, $array){
+    $i = count($array)-1;
+    $j = 0;
+    while($i>=0 && $j<count($array[0])){
+        if($array[$i][$j] > $target)
+            $i--;
+        elseif($array[$i][$j] < $target)
+            $j++;
+        else
+            return 1;
+    }
+    return 0;
 }
 
 
