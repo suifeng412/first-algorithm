053b995835f909f37f43bdfae7318815cc3357e2
forever2015412 <3101424353@qq.com>
2018-01-21 22:12:33 +0800

    新增java代码

---
+++ 03-print_list/print_list.java
@@ -0,0 +1,22 @@
+/**
+*    public class ListNode {
+*        int val;
+*        ListNode next = null;
+*
+*        ListNode(int val) {
+*            this.val = val;
+*        }
+*    }
+*
+*/
+import java.util.ArrayList;
+public class Solution {
+    ArrayList<Integer> arrayList=new ArrayList<Integer>();
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        if(listNode!=null){
+            this.printListFromTailToHead(listNode.next);
+            arrayList.add(listNode.val);
+        }
+        return arrayList;
+    }
+}
