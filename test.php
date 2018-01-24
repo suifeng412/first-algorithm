281e7463e1d953206d7e2273a7ba5c358f24a67c
forever2015412 <3101424353@qq.com>
2018-01-24 16:45:15 +0800

    新增题目

--- test.php
+++ test.php
@@ -1,7 +1,25 @@
 <?php
-$a = 100;
-$b = str_split($a);
+
+$a = "srwthyjuhdIDONNeognalnvslbrp";
+echo "$a<br/>";
+$b = count_chars($a,1);
 echo "<pre>";
 print_r($b);
+echo "</pre>";
+$index = 9999;
+foreach ($b as $k => $v) {
+    if($v == 1){
+        // echo chr($k);
+        $temp = strpos($a, chr($k));
+        if($temp<$index)
+            $index = $temp;
+    }
+}
+echo "<pre>";
+print_r($index);
 echo "</pre>";die;
 
+$c = strpos($a, 'f');
+echo "<pre>";
+print_r($c);
+echo "</pre>";die;
