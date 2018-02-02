31dc810eba045a7df9af74e2e8d619ce43ea9a32
forever2015412 <3101424353@qq.com>
2018-02-02 23:08:53 +0800

    新增java答案+解析

--- 07-fibonacci/fibonacci.php
+++ 07-fibonacci/fibonacci.php
@@ -2,18 +2,16 @@
 
 function Fibonacci($n)
 {
-    $value=0;
-    if($n==0){
-        $value = 0;
+    if($n<=0){
+        $f1 = 0;
     }else if($n==1||$n==2){
-        $value = 1;
+        $f1 = 1;
     }else{
         $f1 = 1; $f2 = 1;
-        for($i=3;$i<=$n;$i++){
-            $value = $f1+$f2;
-            $f2 = $f1;
-            $f1 = $value;
+        while ($n-- > 2) {
+            $f1 += $f2;
+            $f2 = $f1-$f2;
         }
     }
-    return $value;       
+    return $f1;
 }
